045dd3e2397677bb13d06027228fcbacc9178f15
wjsgus95 <wjsgus95@github.com>
2019-05-13 15:13:09 +0900

    v0.0

--- Backtracker.py
+++ Backtracker.py
@@ -8,17 +8,26 @@ import math
 class Backtracker():
     def __init__(self, json_data) -> None:
         self.trace = json_data["trace"]
-        self.ops = [trace[0] for trace in self.trace]
-        self.stacks = [trace[1] for trace in self.trace]
+
+        self.storage_track = list()
 
         self.stats = Stats.Stats(self)
 
     # Replay the whole trace.
     def run(self) -> None:
-        for op, stack in self.trace:
-            pass
+        for trace in self.trace:
+            op = trace["op"]
+            stack = trace["stack"]
+
+            if op == "SSTORE":
+                self.storage_track.append(stack.pop())
+            elif op == "SLOAD":
+                self.storage_track.append(stack.pop())
+            else:
+                pass
 
         # Print stats after exeuction.
-        self.stats.print_stats()
+        #self.stats.print_stats()
+        print(self.storage_track)
 
     
