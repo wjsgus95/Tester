5bc78ec00557af53c4ed1b96f0918a1ad47a6fa2
wjsgus95 <wjsgus95@github.com>
2019-05-13 14:27:10 +0900

    init

---
+++ Backtracker.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from common import constants
+from common import opcode_values
+
+class Backtracker():
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def fin(self):
+        pass
+
+print(opcode_values.op_dict)
---
+++ Tester.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Tester():
+    def __init__(self):
+        self.backtracker = Backtracker()
+        pass
+
+    def run(self):
+        self.backtracker.run()
+        pass
+
+    def fin(self):
+        self.backtracker.fin()
+        pass
