d0b32b92bdfb7bb3f8c7656ff6575ef8b349054f
heyzude <lhg912@naver.com>
2019-05-15 21:38:42 +0900

    no message

--- traces/my_Backtacker.py
+++ traces/my_Backtacker.py
@@ -4,14 +4,17 @@ from common.constants import *
 
 import Stats
 
+
 class Backtracker():
 	def __init__(self, json_data) -> None:
 		self.trace = json_data["trace"]
 
 		self.storage = dict()
-		self.storage_track = dict()
-
+		self.storage_write_track = dict()
+		self.storage_read_track = dict()
 		self.stats = Stats.Stats(self)
+
+
 
 	# Replay the whole trace.
 	def run(self) -> None:
@@ -20,16 +23,17 @@ class Backtracker():
 		for trace in self.trace:
 			op = trace["op"]
 			stack = [int(elt ,16) for elt in trace["stack"]]
-			self.storage_track[idx] = dict()
+			self.storage_write_track[idx] = dict()
+			self.storage_read_track[idx] = []
 
 			if op == "SSTORE":
 				key = stack.pop()
 				value = stack.pop()
 
 				if key in self.storage:
-					self.storage_track[idx][key] = (self.storage[key], value)
+					self.storage_write_track[idx][key] = (self.storage[key], value)
 				else:
-					self.storage_track[idx][key] = ('empty', value)
+					self.storage_write_track[idx][key] = ('empty', value)
 
 				self.storage[key] = value
 
@@ -37,10 +41,12 @@ class Backtracker():
 			elif op == "SLOAD":
 				key = stack.pop()
 				if key in self.storage:
-					stack.append(self.storage[key])
+					#stack.append(self.storage[key])
+					self.storage_read_track[idx].append(key)
+
 				else:
 					print("SLOAD Error: stack poped key doesn't exist in the storage")
-				
+
 
 			else:
 				pass
@@ -48,7 +54,7 @@ class Backtracker():
 
 		idx += 1
 
-        # Print stats after exeuction.
-        # self.stats.print_stats()
-        # print(self.storage_track)
+	# Print stats after exeuction.
+# self.stats.print_stats()
+# print(self.storage_track)
 
